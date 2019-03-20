730eb12cea316c45aab7ab9aedba662bce2214e7
MEBonham <programmer@mckaybonham.com>
2019-03-20 08:52:21 -0600

    Bug-hunting

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -32,12 +32,12 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
-    dest = (char*) dest;
-    dest = malloc(sizeof(char) * n);
+    char *char_ptr_dest = (char*) dest;
+    // char_ptr_dest = malloc(sizeof(char) * n);
     const char *cptr_src = (char*) src;
     for (int i = 0; i < n; i++)
     {
-        dest[i] = cptr_src[i];
+        char_ptr_dest[i] = cptr_src[i];
     }
 }
 
